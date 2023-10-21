9e0a896c43213b4dd6eb280c4a23d8a545ebf822
anqizhao2048 <anqizhao1024@qq.com>
2023-10-20 21:28:34 -0700

    landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,45 @@
+import * as React from "react";
+import {Button, Grid, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
 function LandingPage() {
+    const navigate = useNavigate();
+
+    const goToRule = () => {
+        navigate("/rule")
+    }
+
+    const goToNormalGame = () => {
+        navigate("/normal-game")
+    }
+
+    const goToHardGame = () => {
+        navigate("/hard-game")
+    }
     return (
         <div>
-           Landing Page
+            <div style={{marginTop: "50px"}}>
+                <Typography variant="h4" color="primary" align="center" gutterBottom>
+                    Wordle
+                </Typography>
+            </div>
+
+            <div style={{marginLeft: "100px", marginTop: "100px", marginRight: "100px",  textAlign: "center"}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <div>
+                            <Button variant="contained" onClick={goToRule}>Rule</Button>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained" onClick={goToNormalGame}>Normal Game</Button>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained" onClick={goToHardGame}>Difficult Game</Button>
+                    </Grid>
+                </Grid>
+            </div>
+
         </div>
     );
 }
