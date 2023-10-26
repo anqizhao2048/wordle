e0f08e6171f31a0f86c7eda7443a93c7cca3598d
赵安祺 <zhaoanqi@kis-MacBook-Pro.local>
2023-10-26 08:09:21 -1000

    word match algorithm optimization

--- src/utils/wordCheck.js
+++ src/utils/wordCheck.js
@@ -1,25 +1,38 @@
-export function wordColorCheck(word, correctWord) {
+export function wordColorCheck(guessWord, correctWord) {
     let colors = Array.from(correctWord).fill("");
+    let matched = Array.from(correctWord).fill(false);
 
-    let chs = word.split("")
+    let chs = guessWord.split("")
 
     for (let i = 0; i < chs.length; i++) {
         if (chs[i] === correctWord[i]) {
             colors[i] = "green";
-        } else if (charExist(correctWord, chs[i], colors)) {
+            matched[i] = true;
+        }
+    }
+
+    for (let i = 0; i < chs.length; i++) {
+        if (colors[i] === "green") {
+            continue;
+        }
+
+        if (charExist(correctWord, guessWord, i, matched)) {
             colors[i] = "yellow";
         } else {
             colors[i] = "grey";
         }
     }
 
+
     return colors
 }
 
-function charExist(word, ch, colors) {
-    let chs = word.split("")
+function charExist(correctWord, guessWord, idx, matched) {
+    let chs = correctWord.split("")
     for (let i = 0; i < chs.length; i++) {
-        if (ch === word[i] && colors[i].length === 0) {
+
+        if (guessWord[idx] === chs[i] && !matched[i]) {
+            matched[i] = true;
             return true;
         }
     }
