7bc63ff195003927ff7e0ab2b3ef8eb315938e77
赵安祺 <zhaoanqi@kis-MacBook-Pro.local>
2023-10-26 07:35:13 -1000

    word check and paint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import {Button, TextareaAutosize, Container, Grid} from '@mui/material';
+import {Button, TextareaAutosize, Container, Grid, Typography} from '@mui/material';
 import {useDispatch, useSelector} from "react-redux";
 import {attempt} from "../reducer/gameReducer";
+import {Box, Modal} from "@mui/joy";
 
 function MyForm() {
     const [text, setText] = useState('');
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     const letterNumber  = useSelector((state) => state.gameReducer.letterNumber)
 
@@ -13,39 +17,69 @@ function MyForm() {
         event.preventDefault();
 
         if (text.length !== letterNumber) {
-            alert('please input correct length of the word')
+            handleOpen()
         } else {
             dispatch(attempt(text));
         }
 
     };
 
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
     return (
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={4}></Grid>
-            <Grid item xs={4}>
-                <Container>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <TextareaAutosize
-                                minRows={1}
-                                maxRows={1}
-                                placeholder="Please input your guess..."
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                                style={{ width: '100%', padding: '10px' }}
-                            />
+        <div>
+            <div>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <div style={{textAlign: "center"}}>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                Please input a word of {letterNumber} letters
+                            </Typography>
                         </div>
-                        <div style={{ marginTop: '20px', textAlign: "center" }}>
-                            <Button variant="contained" color="primary" type="submit">
-                                Submit
-                            </Button>
-                        </div>
-                    </form>
-                </Container>
+                        <div style={{textAlign: "center", marginTop: "5px"}}><Button onClick={handleClose}> close</Button></div>
+                    </Box>
+                </Modal>
+            </div>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
+                    <Container>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <TextareaAutosize
+                                    minRows={1}
+                                    maxRows={1}
+                                    placeholder="Please input your guess..."
+                                    value={text}
+                                    onChange={(e) => setText(e.target.value)}
+                                    style={{ width: '100%', padding: '10px' }}
+                                />
+                            </div>
+                            <div style={{ marginTop: '20px', textAlign: "center" }}>
+                                <Button variant="contained" color="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </div>
+                        </form>
+                    </Container>
+                </Grid>
+                <Grid item xs={4}></Grid>
             </Grid>
-            <Grid item xs={4}></Grid>
-        </Grid>
+        </div>
     );
 }
 
--- src/utils/wordCheck.js
+++ src/utils/wordCheck.js
@@ -1,24 +1,25 @@
 export function wordColorCheck(word, correctWord) {
-    let colors = [];
+    let colors = Array.from(correctWord).fill("");
 
     let chs = word.split("")
+
     for (let i = 0; i < chs.length; i++) {
         if (chs[i] === correctWord[i]) {
-            colors.push("green");
-        } else if (charExist(correctWord, chs[i])) {
-            colors.push("yellow");
+            colors[i]("green");
+        } else if (charExist(correctWord, chs[i], colors)) {
+            colors[i]("yellow");
         } else {
-            colors.push("grey")
+            colors[i]("grey")
         }
     }
 
     return colors
 }
 
-function charExist(word, ch) {
+function charExist(word, ch, colors) {
     let chs = word.split("")
     for (let i = 0; i < chs.length; i++) {
-        if (ch === word[i]) {
+        if (ch === word[i] && colors[i].length === 0) {
             return true;
         }
     }
